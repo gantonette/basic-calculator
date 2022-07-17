9c8fdc450aa0535884be1f27469194a568927d25
gantonette <62356789+gantonette@users.noreply.github.com>
2022-07-18 03:25:00 +1200

    added BMI calculator

--- src/App.js
+++ src/App.js
@@ -1,12 +1,64 @@
 import { useState } from 'react';
 
 
+
 function App() {
   const [calc, setCalc] = useState("");
   const [result, setResult] = useState("");
   const [total, setTotal] = useState("");
 
   const ops = ["+", "-", "*", "/", "."];
+
+
+
+  // state (BMI)
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [bmi, setBmi] = useState('');
+  const [message, setMessage] = useState('');
+  
+  // show image based on bmi calculation
+  let imgSrc = '';
+
+  if (bmi < 1) {
+    imgSrc = null;
+  } else {
+    if (bmi < 25) {
+      imgSrc = null;
+    } else if (bmi >= 25 && bmi <30) {
+      imgSrc = null;
+    }
+  }
+
+  let calcBmi = (event) => {
+    event.preventDefault();
+
+    if (weight === 0 || height === 0) {
+      setMessage('Please enter your weight and height');
+      
+    } else {
+      let bmi = (weight / (height * height));
+      setBmi(bmi.toFixed(2));
+
+      // Logic for message
+      if (bmi < 25) {
+        setMessage('You are underweight');
+      } else if (bmi >= 25 && bmi < 30) {
+        setMessage('You are at a healthy weight');
+      } else {
+        setMessage('You are overweight');
+      }
+    }
+  }
+
+  let reload = () => {
+    window.location.reload();
+  }
+
+
+
+
+
 
   const updateCalc = value => {
     if (
@@ -68,6 +120,40 @@ function App() {
 
   return (
     <div className="App">
+
+
+      {/* bmi-calculator */}
+      <div className="bmi-container">
+        <h2 className='bmiCalcCenter'>BMI Calculator</h2>
+        <form onSubmit={calcBmi}>
+          <div>
+            <label>
+              Weight (kgs)</label>
+            <input value={weight} onChange={(event) => setWeight(event.target.value)} />
+          </div>
+          <div>
+            <label>
+              Height (meters)</label>
+            <input value={height} onChange={(event) => setHeight(event.target.value)} />
+          </div>
+          <div>
+            <button className='btn' type='submit'>Submit</button>
+            <button className='btn btn-outline' onClick={reload} type='submit'>Reload</button>
+          </div>
+        </form>
+
+        <div className='center'>
+          <h3>Your BMI is: {bmi}</h3>
+          <p>{message}</p>
+        </div>
+
+        <div className='img-container'>
+          <img src={imgSrc} alt=''></img>
+        </div>
+      </div>
+
+    
+      {/* basic calculator */}  
       <div className="calculator"> 
         <div className="display">
           { result ? <span> ({result}) </span> : '' }
